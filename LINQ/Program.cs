9f009d7c5b892abd08b9b20dc55387d3c6e83fb0
Logan Akins <lakins2@gmail.com>
2025-04-17 16:17:39 -0400

    Linq - Exercise 2 Completion

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LINQ
+﻿using Console = System.Console;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;  
+namespace LINQ_Exercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Legend of Zelda",
+                "Throne and Liberty",
+                "Runescape",
+                "Final Fantasy",
+                "Centipede",
+                "Pac-Man"
+            };
+// order by length of game names
+            var orderedGames = videoGames.OrderBy(game => game.Length).ToList();
+            //print sorted list to console
+            Console.WriteLine("Video games ordered by length of name: ");
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
